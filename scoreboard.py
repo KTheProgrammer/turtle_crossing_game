6743c500286d4edd3f772ba88626426b6bf9b336
Kevio McClellan <57043715+KTheProgrammer@users.noreply.github.com>
2022-04-02 11:53:59 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -9,16 +9,29 @@ class Scoreboard(Turtle):
         self.penup()
         self.hideturtle()
         self.speed("fastest")
-        self.goto(-230, 260)
+        self.goto(0, 260)
         self.scored = 1
-        self.write(f"Level: {self.scored}", align="center", font=FONT)
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
+        self.update()
 
     def new_level(self):
         self.scored += 1
         self.clear()
-        self.write(f"Level: {self.scored}", align="center", font=FONT)
+        self.write(f"Level: {self.scored} Highest Level: {self.high_score}", align="center", font=FONT)
+
+    def update(self):
+        self.clear()
+        self.write(f"Level: {self.scored} Highest Level: {self.high_score}", align="center", font=FONT)
 
     def game_over(self):
         self.penup()
         self.goto(0, 0)
         self.write(f"Game Over", align="center", font=FONT)
+
+    def set_high_score(self):
+        if self.scored > self.high_score:
+            self.high_score = self.scored
+            with open("data.txt", mode="w") as file:
+                file.write(f"{self.high_score}")
+                self.update()
